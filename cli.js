039405fb86cf15d786ce8904584697903f7aedab
ndxbn <sitri.ndxbn@gmail.com>
2019-04-07 02:49:58 +0900

    chore: create-ndxbn と @ndxbn/create の棲み分け対応

--- cli.js
+++ cli.js
@@ -1,2 +1,26 @@
 #!/usr/bin/env node
-console.log("This command does nothing!! Use `npm init @ndxbn`");
+
+// initial repository
+/// unless exists README.md, create it
+/// unless exists LICENSE file, create it
+/// create CODE_OF_CONDUCT.md
+/// create .github/ directory // GitHub specific
+/// create .gitignore
+/// create .travis.yml
+/// create .editorconfig
+/// create renovate.json
+
+// create package.json
+/// scripts
+
+// install typescript
+// create tsconfig.json
+// create "src/" directory
+// create "src/{index.ts, cli.ts, server.ts}
+
+// install jest
+// create jest.config.js
+
+// install prettier
+
+// install eslint
