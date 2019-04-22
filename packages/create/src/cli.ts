50b803fde2efc9b601de119d1f81423b522b1b40
ndxbn <sitri.ndxbn@gmail.com>
2019-04-22 13:26:39 +0900

    feat: rewrite @ndxbn/create package

---
+++ packages/create/src/cli.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import * as path from "path";
+import * as childProcess from "child_process";
+import Application from "./";
+
+const cwd = process.cwd();
+const app = new Application(process.cwd());
+
+// create package.json
+app.template("package.json", { name: path.basename(cwd) });
+/// install dependencies
+/// nothing
+/// install devDependencies
+childProcess.execSync(
+  "npm install -D " +
+    [
+      "@types/node",
+      "@types/jest",
+      "jest",
+      "npm-run-all",
+      "ts-jest",
+      "typescript"
+    ].join(" ")
+);
+
+// install typescript
+// create tsconfig.json
+app.template("tsconfig.json", {});
+// create "src/" directory
+app.mkdir("src");
+// create "src/{index.ts, cli.ts}
+app.touch("src/index.ts");
+app.touch("src/cli.ts");
