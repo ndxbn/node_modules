5957a9d5504643130bf29df1ba5a55362ffcac13
ndxbn <sitri.ndxbn@gmail.com>
2019-05-20 07:59:14 +0900

    feat(logger): add TestLogger

---
+++ packages/@ndxbn/logger/src/Logger/TestLogger.spec.ts
@@ -0,0 +1,90 @@
+import TestLogger from "./TestLogger";
+
+describe("hasRecords method", () => {
+  test("should return true when have records", async () => {
+    const logger = new TestLogger();
+    await logger.info("foo");
+
+    expect(logger.hasRecords("info")).toBeTruthy();
+  });
+
+  test("should return false when does not have records", () => {
+    const logger = new TestLogger();
+
+    expect(logger.hasRecords("info")).toBeFalsy();
+  });
+  test("should return false when does not records but have other loglevel record,", async () => {
+    const logger = new TestLogger();
+    await logger.debug("foo");
+
+    expect(logger.hasRecords("info")).toBeFalsy();
+  });
+});
+
+describe("hasRecord method", () => {
+  test("should return true when have records", async () => {
+    const logger = new TestLogger();
+    await logger.info("foo");
+
+    expect(logger.hasRecord({ level: "info", message: "foo", context: {} })).toBeTruthy();
+  });
+
+  test("should return false when does not have records", () => {
+    const logger = new TestLogger();
+
+    expect(logger.hasRecord({ level: "notice", message: "foo", context: {} })).toBeFalsy();
+  });
+  test("should return false when does not have records: loglevel", async () => {
+    const logger = new TestLogger();
+    await logger.info("foo");
+
+    expect(logger.hasRecord({ level: "warning", message: "foo", context: {} })).toBeFalsy();
+  });
+  test("should return false when does not have records: message", async () => {
+    const logger = new TestLogger();
+    await logger.info("foo");
+
+    expect(logger.hasRecord({ level: "info", message: "bar", context: {} })).toBeFalsy();
+  });
+  test("should return false when does not have records: context", async () => {
+    const logger = new TestLogger();
+    await logger.info("foo");
+
+    expect(logger.hasRecord({ level: "info", message: "foo", context: { bar: "baz" } })).toBeFalsy();
+  });
+
+});
+
+describe("hasRecordThatContains method", () => {
+  test("should return true when have records", async () => {
+    const logger = new TestLogger();
+    await logger.info("foo bar");
+
+    expect(logger.hasRecordThatContains("foo", "info")).toBeTruthy();
+    expect(logger.hasRecordThatContains("o b", "info")).toBeTruthy();
+  });
+
+  test("should return false when does not have records", async () => {
+    const logger = new TestLogger();
+    await logger.info("foo bar");
+
+    expect(logger.hasRecordThatContains("baz", "info")).toBeFalsy();
+    expect(logger.hasRecordThatContains("ob", "info")).toBeFalsy();
+  });
+});
+describe("hasRecordThatMatches method", () => {
+  test("should return true when have records", async () => {
+    const logger = new TestLogger();
+    await logger.info("foo bar");
+
+    expect(logger.hasRecordThatMatches(/^foo.*/, "info")).toBeTruthy();
+    expect(logger.hasRecordThatMatches(/bar$/, "info")).toBeTruthy();
+  });
+
+  test("should return false when does not have records", async () => {
+    const logger = new TestLogger();
+    await logger.info("foo bar");
+
+    expect(logger.hasRecordThatMatches(/zzz/, "info")).toBeFalsy();
+  });
+});
---
+++ packages/@ndxbn/logger/src/Logger/TestLogger.ts
@@ -0,0 +1,57 @@
+import { AbstractLogger } from "../";
+import { LogLevel, Context } from "../constants";
+import * as assert from "assert";
+
+type Record = {
+  "level": LogLevel,
+  "message": string,
+  "context": Context
+}
+/**
+ * Used for testing purposes.
+ *
+ * It records all records and gives you access to them for verification.
+ */
+export default class TestLogger extends AbstractLogger {
+  public records: Record[] = [];
+
+  public async log(level: LogLevel, message: string, context: Context = {}): Promise<void> {
+    this.records.push({ level, message, context });
+  }
+
+  public hasRecords(level: LogLevel): boolean {
+    return this.records.find(record => record.level === level) != undefined;
+  }
+
+  public hasRecord(record: Record): boolean {
+    return this.records.find(haystack => {
+      // "level" and "message" is primitive, but "context" is object.
+      if (haystack.level === record.level
+        && haystack.message === record.message) {
+        try {
+          assert.deepStrictEqual(haystack.context, record.context);
+          return true;
+        } catch (e) {
+          return false;
+        }
+      }
+      return false;
+    }) != undefined;
+  }
+
+  public hasRecordThatContains(message: string, level: LogLevel): boolean {
+    return this.records.find(
+      record => record.level === level && record.message.includes(message)
+    ) != undefined;
+  }
+
+  public hasRecordThatMatches(regex: RegExp, level: LogLevel): boolean {
+    return this.records.find(
+      record => record.level === level && regex.test(record.message)
+    ) != undefined;
+  }
+
+  public reset(): void {
+    this.records = [];
+  }
+}
