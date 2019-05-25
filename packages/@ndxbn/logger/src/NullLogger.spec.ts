32f478ee12b6983fbb5d0085ddaee3e64ca10e9a
ndxbn <sitri.ndxbn@gmail.com>
2019-05-26 03:06:41 +0900

    chore(logger): add NullLogger.spec.ts

---
+++ packages/@ndxbn/logger/src/NullLogger.spec.ts
@@ -0,0 +1,7 @@
+import NullLogger from "./NullLogger";
+
+test("NullLogger should have log method", async () => {
+  const logger = new NullLogger();
+
+  expect(await logger.log()).toBeUndefined();
+});
