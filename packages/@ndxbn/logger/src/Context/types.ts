a1b87af49d0554a05c9836558ef24b84b1d14230
ndxbn <sitri.ndxbn@gmail.com>
2019-05-26 06:50:48 +0900

    chore(logger): Change Context Type
    BREAKING CHANGE:

--- packages/@ndxbn/logger/src/Context/types.ts
+++ packages/@ndxbn/logger/src/Context/types.ts
@@ -1,3 +1,7 @@
+/**
+ * key names SHOULD be composed only of
+ *   the characters `A-Z`, `a-z`, `0-9`, underscore `_`, and period `.` .
+ */
 export type Context = {
-  [k: string]: { toString(): string };
+  [k: string]: string;
 };
