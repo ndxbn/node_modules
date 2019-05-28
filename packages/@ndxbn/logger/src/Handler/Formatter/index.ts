2bdb879778c55d573c3efae550d30584db971247
ndxbn <sitri.ndxbn@gmail.com>
2019-05-28 17:15:07 +0900

    chore(logger): update index.ts

--- packages/@ndxbn/logger/src/Handler/Formatter/index.ts
+++ packages/@ndxbn/logger/src/Handler/Formatter/index.ts
@@ -2,3 +2,4 @@ export { default as FormatterInterface } from "./FormatterInterface";
 
 // concrete formatters
 export { default as RawFormatter } from "./RawFormatter";
+export { default as NoopFormatter } from "./RawFormatter";
