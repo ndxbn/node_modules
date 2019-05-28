cf7870f0563ca4617e530dacf7e2e860eeba8ff9
ndxbn <sitri.ndxbn@gmail.com>
2019-05-28 17:14:08 +0900

    chore(logger): comment

--- packages/@ndxbn/logger/src/Handler/Formatter/RawFormatter.ts
+++ packages/@ndxbn/logger/src/Handler/Formatter/RawFormatter.ts
@@ -1,5 +1,8 @@
 import FormatterInterface from "./FormatterInterface";
 
+/**
+ * Return message as is formatter
+ */
 export default class RawFormatter implements FormatterInterface{
   public format(message: string): string {
     return message;
