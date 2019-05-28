23a6e2a4a9ae83eb4115eea3145f934f1249f61f
ndxbn <sitri.ndxbn@gmail.com>
2019-05-29 02:54:28 +0900

    feat(logger): remove NoopFormatter

--- packages/@ndxbn/logger/src/Handler/Formatter/index.ts
+++ packages/@ndxbn/logger/src/Handler/Formatter/index.ts
@@ -2,4 +2,3 @@ export { default as FormatterInterface } from "./FormatterInterface";
 
 // concrete formatters
 export { default as RawFormatter } from "./RawFormatter";
-export { default as NoopFormatter } from "./RawFormatter";
