c778f255b666699024095460de96ea88c89f6a44
ndxbn <sitri.ndxbn@gmail.com>
2019-05-29 02:55:32 +0900

    chore(logger): fix Context type

--- packages/@ndxbn/logger/src/Context/types.ts
+++ packages/@ndxbn/logger/src/Context/types.ts
@@ -2,6 +2,4 @@
  * key names SHOULD be composed only of
  *   the characters `A-Z`, `a-z`, `0-9`, underscore `_`, and period `.` .
  */
-export type Context = {
-  [k: string]: string;
-};
+export type Context = Map<string, string>;
