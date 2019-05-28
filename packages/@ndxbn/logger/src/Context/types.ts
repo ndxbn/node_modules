42115a00910b85b756b4d2211c92503feb66e572
ndxbn <sitri.ndxbn@gmail.com>
2019-05-29 03:43:34 +0900

    chore(logger): comment

--- packages/@ndxbn/logger/src/Context/types.ts
+++ packages/@ndxbn/logger/src/Context/types.ts
@@ -1,5 +1,4 @@
 /**
- * key names SHOULD be composed only of
- *   the characters `A-Z`, `a-z`, `0-9`, underscore `_`, and period `.` .
+ * key names SHOULD be composed only of the characters `A-Z`, `a-z`, `0-9`, underscore `_`, and period `.` .
  */
 export type Context = Map<string, string>;
