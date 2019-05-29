5fabe13f91de40f45a89b145445c796c46bcb519
ndxbn <sitri.ndxbn@gmail.com>
2019-05-29 12:50:22 +0900

    feat(logger): update index

--- packages/@ndxbn/logger/src/index.ts
+++ packages/@ndxbn/logger/src/index.ts
@@ -1,7 +1,8 @@
 // Logger
+export { default as default } from "./Logger";
 export { default as LoggerInterface } from "./LoggerInterface";
-export { default as AbstractLogger } from "./LoggerBase";
-export * from "./constants";
+export { default as LoggerBase } from "./LoggerBase";
+export { LogLevel } from "./constants";
 
 // concrete Logger
 export { default as NullLogger } from "./NullLogger";
@@ -9,3 +10,9 @@ export { default as NullLogger } from "./NullLogger";
 // LoggerAware
 export { default as LoggerAwareInterface } from "./LoggerAwareInterface";
 // export * from "./LoggerAwareTrait";
+
+// Handler and Formatter
+export * from "./Handler";
+
+// Context
+export * from "./Context";
