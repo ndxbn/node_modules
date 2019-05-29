3dc5706ffa262b4f5c1bd2992a8907199fcaf815
ndxbn <sitri.ndxbn@gmail.com>
2019-05-29 12:43:07 +0900

    feat(logger): ConsoleLog Handler

---
+++ packages/@ndxbn/logger/src/Handler/ConsoleLogHandler.ts
@@ -0,0 +1,25 @@
+import { Context } from "../Context";
+import { FormatterInterface, RawFormatter } from "./Formatter";
+import HandlerInterface from "./HandlerInterface";
+import Interpolator from "./Interpolator";
+
+/**
+ * output with `console.log()`
+ */
+export default class ConsoleLogHandler implements HandlerInterface {
+  protected readonly formatter: FormatterInterface = new RawFormatter();
+
+  protected static get contextBase(): Context {
+    return new Map([["datetime", (new Date().toISOString())]]);
+  }
+
+  public async log(message: string, context: Context): Promise<void> {
+    // merge
+    const contextExtended : Context = new Map([...ConsoleLogHandler.contextBase, ...context]);
+
+    const logMessage = Interpolator.interpolate(message, contextExtended);
+    console.log(logMessage);
+
+    return;
+  }
+}
--- packages/@ndxbn/logger/src/Handler/index.ts
+++ packages/@ndxbn/logger/src/Handler/index.ts
@@ -1,3 +1,9 @@
+// handlers
+// interface
 export { default as HandlerInterface } from "./HandlerInterface";
+// concretes
+export { default as ConsoleLogHandler } from "./ConsoleLogHandler";
+export { default as TestHandler } from "./TestHandler";
 
+// formatters
 export * from "./Formatter";
